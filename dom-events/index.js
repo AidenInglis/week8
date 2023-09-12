3e8edd734b418fe1eee0f8cc101312b177a042f3
AidenInglis <aiden-inglis@live.nmit.ac.nz>
2023-09-12 12:49:53 +1200

    adding data array usoing item.textContent new thingy

--- dom-events/index.js
+++ dom-events/index.js
@@ -1,27 +1,19 @@
 // NOTE
+let menu = document.getElementById('menu');
+let items = menu.getElementsByClassName('item');
+
+
+let data = [].map.call(items, (item) => item.textContent) //[java,css, html, typescript]
+
 
 //const p = document.getElementById('message');//if nothing is specified here then nothing will be returned
 //console.log(p);
 
-let btn = document.getElementById('btnCount')
-btn.addEventListener('click', () => {
-    let hTwoHeader = document.getElementsByTagName('h2')
-    alert(`The number of H2 tags are : ${hTwoHeader.length}`)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// let btn = document.getElementById('btnCount')
+// btn.addEventListener('click', () => {
+//     let hTwoHeader = document.getElementsByTagName('h2')
+//     alert(`The number of H2 tags are : ${hTwoHeader.length}`)
+// })
 
 // let btn = document.getElementById('btnRadio');
 // let output = document.getElementById('output');
